ee1ea2db26e4457301cc34d6598203da4c4aa854
dhieb-dev <dhiebrouissidev@gmail.com>
2024-10-12 19:26:20 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -9,6 +11,7 @@ module.exports = {
     extend: {
        backgroundImage: {"backgroundItem": "url('./static/images/bg-pattern.png')"},
     },
+  darkMode: "class",
   },
   plugins: [],
 }
