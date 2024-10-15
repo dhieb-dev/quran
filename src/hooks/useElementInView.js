20694b71150ac517642cffd352ed577029553e9e
dhieb-dev <dhiebrouissidev@gmail.com>
2024-10-16 01:27:00 +0200

    Update useElementInView.js

--- src/hooks/useElementInView.js
+++ src/hooks/useElementInView.js
@@ -12,9 +12,9 @@ export const useElementInView = () => {
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          entry.target.classList.remove("opacity-0", "scale-50", "mt-10")
+          entry.target.classList.remove("opacity-0", "scale-50")
         } else {
-          entry.target.classList.add("opacity-0", "scale-50", "mt-10")
+          entry.target.classList.add("opacity-0", "scale-50")
         }
       })
     }, option);
